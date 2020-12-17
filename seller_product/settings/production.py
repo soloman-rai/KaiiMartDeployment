f57ffd7011b8cc7fa9ad2cf72f2b313fb1ad649b
Soloman Rai <soloman_rai@Solomans-MBP.domain.name>
2020-12-17 16:10:00 +0545

    'changes on settings.py

--- seller_product/settings/production.py
+++ seller_product/settings/production.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-# import dj_database_url
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kaiimart.com', 'www.kaiimart.com']
 
 
 # Application definition
